e2b4039c27cf4ca8d137c0e856cb025628d7e591
Vikas <77691137+vikas528@users.noreply.github.com>
2021-10-24 20:14:02 +0530

    Update ques_3.c

--- ques_3.c
+++ ques_3.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
-#define N 500000
+#define N 50000
 
-void sieveOfErastosthenes(long long int arr[], long long int n){
+void sieveOfErastosthenes(long long int arr[], long long int brr[], long long int n){
     for(long long int i=2; i<n; i++){
-        if(arr[i]!=i)
+        if(arr[i]!=i){
+            brr[i]=0;
             continue;
+        }
         for(long long int j=i; j<=n; j+=i)
-            if(arr[j]==j)
+            if(arr[j]==j){
                 arr[j]=i;
+                int k=j+i;
+                if(k<=n)
+                    brr[k]=0;
+            }
     }
 }
 
 int main()
 {
-    long long int arr[N];
-    for(long long int i=0; i<N; i++)
+    long long int arr[N], brr[N];
+    for(long long int i=0; i<N; i++){
         arr[i]=i;
+        brr[i]=i;
+    }
     printf("Enter the number:\n");
-    sieveOfErastosthenes(arr, N);
-    // for(int i=0; i<N; i++)
-    //     printf("%d ", arr[i]);
+    sieveOfErastosthenes(arr, brr, N);
     long long int n;
     scanf("%lld", &n);
-    printf("Prime factors of %lld will be:\n", n);
     
-    while(n>1){
-        printf("%lld ", arr[n]);
-        n/=arr[n];
-        if(n==1)
-            break;
-    }
+    for(int i=2; i<=n; i++)
+        if(brr[i])
+            printf("%d ", brr[i]);
     return 0;
 }
